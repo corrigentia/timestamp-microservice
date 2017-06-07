d12a340dbd16ce98067073160f77a4776d5c59b2
Ryan <coryan25@gmail.com>
2017-06-07 20:24:54 +0000

    configure for deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/:time', function (req, res, next) {
+    'use strict';
+    // res.send(req.params.time);
+    // res.render('index', { time: req.params.time });
+    // var data = { time: req.params.time };
+    // res.json(data);
+
+    function unixToNatural (unix) {
+        var date = new Date(unix * 1000);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var month = months[date.getMonth];
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        var result = month + ' ' + day + ', ' + year;
+        return result;
+    }
+
+    if (!isNaN(req.params.time)) {
+        var result = unixToNatural(req.params.time);
+        var data = { unix: req.params.time, natural: result };
+        res.json(data);
+    } else {
+        var natural = new Date(req.params.time);
+        if (!isNaN(natural)) {
+            var unix = natural / 1000;
+            var data = { unix: unix, natural: req.params.time };
+            res.json(data);
+        }
+        else {
+            res.json({ unix: null, natural: null });
+        }
+    }
 });
 
 module.exports = router;
